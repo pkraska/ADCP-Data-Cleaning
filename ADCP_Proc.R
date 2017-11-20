84ac2b700b6f61139c13ebfc200034fbe13ce806
Kraska <KRASKAPE@dfo-mpo.ca>
2017-11-20 10:56:12 -0400

    Updated script using dplyr. got spacing of headers using RLE, cleaned data so it is ready for header info to be added back.

--- ADCP_Proc.R
+++ ADCP_Proc.R
@@ -1,6 +1,35 @@
-install.packages("readr")
+## If you haven't installed previously, uncomment next three lines lines and run
+# install.packages("readr")
+# install.packages("tidyr")
+# install.packages("dplyr")
+# install.packages("reshape2")
+
 library("readr")
 library("tidyr")
 library("dplyr")
+library("reshape2")
+
 # Read csv file of data into R
-data <- read.csv("data.csv", header = FALSE)
+data <- read_csv("data.csv", col_names = FALSE)
+
+# Get header information from file
+header <- data %>%
+  filter(!is.na(data[,1]))
+
+# testing metling and recasting header rows into single row
+test.head <- header[1:6,]
+mat.head <- as.matrix(test.head)
+data.head <- as.vector(mat.head)
+# get header spacing info
+rle.data <- rle(is.na(data$X1))   #rle, creates list of lengths and values that repeat
+
+# remove the first element of the lists
+na.len <- rle.data$lengths[-1]
+na.values <- rle.data$values[-1]
+
+# Remove header info from file to create flat file of just measurments
+r.data <- data %>%
+  filter(is.na(X1)) %>%               #filter out all header information
+  slice(-1) %>%                       #remove first row in datafile
+  select(-1)                          #remove first column that is empty
+
