432cacba5eba5b0a595f79391b2f0aeaedb78cbb
Kraska <KRASKAPE@dfo-mpo.ca>
2018-05-28 09:40:17 -0300

    Added COERS and RiverRay options to the script

--- ADCP_processing.R
+++ ADCP_processing.R
@@ -1,4 +1,5 @@
 # Function to process ADCP data into two flat files (header/data)
+x <- "vr065_553.adcp" 
 
 ADCPproc <- function(x, unit = "RiverRay") {
   # x is the data you wish to process unit is the type of unit you used to collect
@@ -6,78 +7,76 @@ ADCPproc <- function(x, unit = "RiverRay") {
   # this script.
   
   require(tidyverse)
+  require(data.table)
 
-  x <- "vr065_553.adcp"    
-  
   # read data as rows of characters
-  data <- data.frame(readLines(x))
+    data <- readLines(x) %>%
+    trimws(., which = 'both') %>%
+    data.table(.) %>%
+    select(read = 1) %>%
+    separate(read, into = paste("V", c(1:13), sep = ""), sep = "\\s+")
+    
   
   # Header rows are identified by the 'cm' and 'BT' in the character strings.
   # RiverRay header is 6 lines, while the COERS ADCP is 5.
-  header <- regexpr("[a-z]+", data[[1]]) > 0
+  regex.header <- regexpr("[a-z]+", readLines(x)) > 0
+  header <- rep(NA, length(regex.header))
+  
+  # Loop over the dataframe looking for the head == TRUE and then change the
+  # previous 4 header items to TRUE as well.
+  if (unit == "COERS") {
+    for (i in 1:length(header)) {
+      if (regex.header[i] == TRUE) {
+        header[(i - 4):i] <- TRUE
+      } else {
+        header[i] <- FALSE
+      }
+    }
+  }
   
   data$header <- header
-
-  # Loop over the dataframe looking for the head == TRUE and then change the previous 4 header items to TRUE as well.
-  for (i in 1:length(data$header)){
-    if(data$header[i] == TRUE){
-      data$header[(i-4):i] <- TRUE
-    } 
-  }
-
-
-  header.rle <- unlist(rle(data$header)$lengths)
-  header.logical <- unlist(rle(data$header)$values)
-
-  header <- data.frame(header.rle, header.logical)
-  rownames(header) <- NULL
   
-   if (unit == "RiverRay") {
-    
-    # create logical vector of whether or not the row is part of the header or not
-    # header.rows <- substr(data, 0, 1) != " "
-    
-    # read data as text file, creating a character column for data, then removing the
-    # first two blank rows
-    tab <- read_table(x, col_names = FALSE)
-    
-    # Add header.rows information to data so we can eventually filter by column
-    tab$header <- header.rows
-    colnames(tab) <- c("char", "header")
-    
-    # use rle (Run Length Encoding) function from base r in order to figure out the
-    # ID of each ensemble
-    tab.rle <- rle(tab$header)
-    
-    # create dataframe of the length and header/data sections.  TRUE = header, FALSE
-    # = data, remove first row, and filter to only lengths of actual data
-    df.rle <- data.frame(lengths = unlist(tab.rle$lengths), values = unlist(tab.rle$values)) %>% 
-      slice(2:n()) %>% filter(values == FALSE)
-    
-    # create a reference for which header information UID is needed to be pasted to
-    # each data row
-    UID.ref <- rep(1:length(df.rle$lengths), df.rle$lengths)
-    
-    # Filter data into df.header remove header column, and separate char into columns
-    df.header <- tab %>% filter(header == TRUE) %>% 
-      select(char) %>% 
-      separate(char, into = paste("x", c(1:13), sep = "."), sep = "\\s+")
-    
-    # Consolidate header information from 6 rows into 1 using cbind (column bind)
-    header.long <- df.header[seq(from = 1, to = nrow(df.header), by = 6), ] %>% 
-      cbind(df.header[seq(from = 2, to = nrow(df.header), by = 6), ]) %>% 
-      cbind(df.header[seq(from = 3, to = nrow(df.header), by = 6), ]) %>% 
-      cbind(df.header[seq(from = 4, to = nrow(df.header), by = 6), ]) %>% 
-      cbind(df.header[seq(from = 5, to = nrow(df.header), by = 6), ]) %>% 
-      cbind(df.header[seq(from = 6, to = nrow(df.header), by = 6), ])
+  if (unit == "RiverRay") {
+    for (i in 1:length(data$header)) {
+      if (data$header[i] == TRUE) {
+        data$header[(i - 5):i] <- TRUE
+      }
+    }
+  }
+  
+  # Create data frame of RLE (run length encoding) of the TRUE/FALSE header for the
+  # length of each TRUE/FALSE section and trim off the first FALSE as it is part of
+  # discarded data
+  header.rle <- data.frame(lengths = unlist(rle(data$header)$lengths), values = unlist(rle(data$header)$values)) %>% 
+    slice(2:n())
+  
+  # Create a unique ID for each section of data to link to the header section
+  UID.ref <- rep(1:length(header.rle$lengths[header.rle$values == FALSE]), header.rle$lengths[header.rle$values == FALSE])
+  
+  ensemble.data <- data %>%
+    slice(4:n()) %>%
+    filter(header == FALSE) %>%
+    mutate(UID = UID.ref) %>%
+    select(UID, everything()) 
+  
+  ensemble.header.data <- data %>%
+    slice(4:n()) %>%
+    filter(header == TRUE) %>%
+    select(-header)
+  
+  if (unit == "COERS") {
+    ensemble.header <- ensemble.header.data[seq(from = 1, to = nrow(ensemble.header.data), by = 5), ] %>% 
+      cbind(ensemble.header.data[seq(from = 2, to = nrow(ensemble.header.data), by = 5), ]) %>% 
+      cbind(ensemble.header.data[seq(from = 3, to = nrow(ensemble.header.data), by = 5), ]) %>% 
+      cbind(ensemble.header.data[seq(from = 4, to = nrow(ensemble.header.data), by = 5), ]) %>% 
+      cbind(ensemble.header.data[seq(from = 5, to = nrow(ensemble.header.data), by = 5), ])   
     
     # rename column names as they copied the same names each time in previous step
-    colnames(header.long) <- paste("v", seq(from = 1, to = ncol(header.long), 
-                                            by = 1), sep = "")
-    
+    colnames(ensemble.header) <- paste("v", seq(from = 1, to = ncol(ensemble.header), 
+                                                by = 1), sep = "")
     # Remove blank columns, and rename columns to human readable format
-    header <- header.long %>% 
-      select(-22:-26, -32:-39, -45:-52, -58:-65, -72:-78) %>% 
+    ensemble.header.clean <- ensemble.header %>% 
+      select(-22:-26, -33:-39, -49:-52, -55:-65) %>% 
       rename(year = v1, 
              month = v2, 
              day = v3, 
@@ -90,26 +89,41 @@ ADCPproc <- function(x, unit = "RiverRay") {
              pitch = v10, 
              roll = v11, 
              cor_heading = v12, 
-             adcp_temp = v13, 
-             latitude = v40, 
-             longitude = v41) %>% 
+             adcp_temp = v13) %>% 
       mutate(UID = paste(year, month, day, hour, second, hth_second, sep = ".")) %>% 
-      select(UID, everything()) %>% write_csv("ADCP_HEADER.csv")
-    
-    # Filter data into df.data remove header column, and separate char into columns
-    df.data <- tab %>% filter(header == FALSE) %>% 
-      select(char) %>% 
-      slice(2:n()) %>% 
-      separate(char, into = paste("x", c(1:9), sep = "."), sep = "\\s+") %>% 
-      mutate(UID = header$UID[UID.ref]) %>% 
       select(UID, everything()) %>% 
-      write_csv("ADCP_DATA.csv")
-  } 
-  if (unit == "COERS") {
-    
-    }
-  else {
-    message("Haven't finished writing script yet for other Teledyne ADCP units.")
+      write_csv("ADCP_HEADER.csv")
   }
   
+  if (unit == "RiverRay") {
+    ensemble.header <- ensemble.header.data[seq(from = 1, to = nrow(ensemble.header.data), by = 6), ] %>% 
+      cbind(ensemble.header.data[seq(from = 2, to = nrow(ensemble.header.data), by = 6), ]) %>% 
+      cbind(ensemble.header.data[seq(from = 3, to = nrow(ensemble.header.data), by = 6), ]) %>% 
+      cbind(ensemble.header.data[seq(from = 4, to = nrow(ensemble.header.data), by = 6), ]) %>% 
+      cbind(ensemble.header.data[seq(from = 5, to = nrow(ensemble.header.data), by = 6), ]) %>%
+      cbind(ensemble.header.data[seq(from = 6, to = nrow(ensemble.header.data), by = 6), ])
+    
+    ensemble.header.clean <- ensemble.header %>% 
+      select(-22:-26, -32:-39, -45:-52, -58:-65, -72:-78) %>%
+      rename(year = v1,
+             month = v2,
+             day = v3,
+             hour = v4,
+             minute = v5,
+             second = v6,
+             hth_second = v7,
+             ens_number = v8,
+             ens_in_segment = v9,
+             pitch = v10,
+             roll = v11,
+             cor_heading = v12,
+             adcp_temp = v13,
+             
+             latitude = v40,
+             longitude = v41) %>% 
+      mutate(UID = paste(year, month, day, hour, second, hth_second, sep = ".")) %>% 
+      select(UID, everything()) %>% 
+      write_csv("ADCP_HEADER.csv")
+  }
+
 }
